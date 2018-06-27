78de92ffc34c0bafba4aefe2ae6967dacc196827
Lvmmbrkln <j.degueltzl@gmail.com>
2018-06-27 12:21:58 +0200

    pyramide terminée

---
+++ pyramide.rb
@@ -0,0 +1,14 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'etages veux tu ?"
+print "> "
+x = gets.chomp.to_i
+n = 1
+
+while n <= x
+    if n<= x
+    (n == 1)
+       puts ("#" * n).rjust(x)
+    else
+        break
+    end
+   n += 1
+end
